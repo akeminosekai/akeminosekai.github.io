e14f20299778a273d5453c45bc5a93c732cac28e
akeminosekai <akemi.kyosuke@gmail.com>
2019-05-04 16:58:05 +0200

    init

---
+++ posts/2017/09/08/script.js
@@ -0,0 +1,16 @@
+document.getElementById('btAff1').addEventListener('click', function() {
+  document.getElementById('pan1').style.display = 'block'
+  this.style.display = 'none'
+}, false)
+document.getElementById('btAff2').addEventListener('click', function() {
+  document.getElementById('pan2').style.display = 'block'
+  this.style.display = 'none'
+}, false)
+document.getElementById('btAff3').addEventListener('click', function() {
+  document.getElementById('pan3').style.display = 'block'
+  this.style.display = 'none'
+}, false)
+document.getElementById('btAff4').addEventListener('click', function() {
+  document.getElementById('pan4').style.display = 'block'
+  this.style.display = 'none'
+}, false)
